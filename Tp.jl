eb3be5d15a9deee37abfd888419b3fad7865f6b9
damian yjilioff <damianyjilioff@gmail.com>
2024-06-23 17:42:58 -0300

    .

--- Tp.jl
+++ Tp.jl
@@ -41,12 +41,13 @@ end
 
 function stima4(vertices)
     D_Phi = transpose([vertices[2, :] - vertices[1, :]; vertices[4, :] - vertices[1, :]])
-    B = (D_Phi'* D_Phi)
+    print(typeof(D_Phi))
+    B = (D_Phi)'* (D_Phi)
     print(B)
-    C1 = [2 -2; -2  2] * B[1  1] + [3 0; 0 -3] * B[1 2] + [2 1; 1 2] * B[2 2]
-    C2 = [-1 1; 1 -1] .* B[1 1] .+ [-3 0; 0 3] .* B[1 2] .+ [-1 -2; -2 -1] .* B[2 2]
-    M = det(D_Phi) * [C1 C2; C2 C1] / 6
-    return M
+    C1 = [2 -2; -2  2] .* B[1 , 1] + [3 0; 0 -3] .* B[1 ,2] + [2 1; 1 2] .* B[2, 2]
+    #C2 = [-1 1; 1 -1] .* B[1 1] .+ [-3 0; 0 3] .* B[1 2] .+ [-1 -2; -2 -1] .* B[2 2]
+    #M = det(D_Phi) * [C1 C2; C2 C1] / 6
+    #return M
 end
 
 function u_d(x)
@@ -74,42 +75,51 @@ for j in 1:length(elements3[:,1])
     A[indices, indices] += stima3(coordinates[indices, :])
 end
 
-a = [elements4[2, :],:][2,:] 
 
-stima4(coordinates[elements4[2, :],:])
-#stima4(coordinates[[2,1], :])
 
 for j in 1:length(elements4[:,1])
     indices = elements4[j, :]
+    print(coordinates[indices, :])
     A[indices, indices] += stima4(coordinates[indices, :])
 end
-
+display(A)
 # Volume Forces
-for j in 1:size(elements3, 1)
-    b[elements3[j, :]] += det([1 1 1; coordinates[elements3[j, :], :]']) * f(sum(coordinates[elements3[j, :], :], dims=1) / 3) / 6
+for j in 1:length(elements3[1,:])
+    for i in 1:length(elements3[1,:])
+    b[elements3[j, i]] += (det([1 1 1; coordinates[elements3[j, :], :]']) * f(sum(coordinates[elements3[j, :], :], dims=1) / 3) / 6)[1] 
+    end
 end
 
-for j in 1:size(elements4, 1)
-    b[elements4[j, :]] += det([1 1 1; coordinates[elements4[j, 1:3], :]']) * f(sum(coordinates[elements4[j, :], :], dims=1) / 4) / 4
+for j in 1:length(elements3[1,:])
+    for i in 1:length(elements3[1,:])
+    b[elements4[j, i]] += (det([1 1 1; coordinates[elements4[j, 1:3], :]']) * f(sum(coordinates[elements4[j, :], :], dims=1) / 4) / 4)[1]
+    end
 end
 
-# Neumann conditions
-for j in 1:size(neumann, 1)
-    b[neumann[j, :]] += norm(coordinates[neumann[j, 1], :] - coordinates[neumann[j, 2], :]) * g(sum(coordinates[neumann[j, :], :], dims=1) / 2) / 2
-end
+norm(coordinates[neumann[1, :], :] - coordinates[neumann[1, :], :]) 
+
+#g(sum(coordinates[neumann[1, :], :], dims=1) / 2) / 2
+ #Neumann conditions
+(g(sum(coordinates[Integer.(neumann[1, :]), :]) / 2) / 2)
+#for j in 1:length(neumann[1,:])
+#    for i in 1:length(neumann[1,:])
+ #   b[neumann[j, i]] += (norm(coordinates[Integer.(neumann[j, 1]), :] - coordinates[Integer.(neumann[j ,2]), :]) * (g(sum(coordinates[Integer.(neumann[j, :]), :]) / 2) / 2)[1])
+ #   end
+#end
 
 # Dirichlet conditions
 u = spzeros(size(coordinates, 1))
-u[unique(dirichlet)] = u_d(coordinates[unique(dirichlet), :])
+Integer.(dirichlet)
+u[Integer.(unique(dirichlet))] = u_d(coordinates[Integer.(unique(dirichlet)), :])
 b = b - A * u
 
 # <<< method 1
 A = Matrix(A)
 b = Matrix(b)
-b[unique(dirichlet)] = u_d(coordinates[unique(dirichlet), :])
-A[unique(dirichlet), :] .= 0.0
-A[:, unique(dirichlet)] .= 0.0
-temp = unique(dirichlet)
+b[Integer.(unique(dirichlet))] = u_d(coordinates[Integer.(unique(dirichlet)), :])
+A[Integer.(unique(dirichlet)), :] .= 0.0
+A[:, Integer.(unique(dirichlet))] .= 0.0
+temp = Integer.(unique(dirichlet))
 
 for i in 1:length(temp)
     A[temp[i], temp[i]] = 1.0
